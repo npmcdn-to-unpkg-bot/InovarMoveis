c52daf48b9f30d5f5f4d1c0941161541ecfa9bc8
alessandrotraversi <alessandro.traversi@hotmail.it>
2016-03-08 15:30:12 -0300

    concluso jade e impostato elixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,37 @@
+/*
+ |--------------------------------------------------------------------------
+ | Required components
+ |--------------------------------------------------------------------------
+ */
 var elixir = require('laravel-elixir');
-require('laravel-elixir-jade');
+            require('laravel-elixir-jade');
+var gulp = require('gulp');
+var bower = require('gulp-bower');
 
+/*
+ |--------------------------------------------------------------------------
+ | Bower setup
+ |--------------------------------------------------------------------------
+ */
+var bowerDir = './resources/assets/bower/';
+
+gulp.task('bower', function() {
+    return bower({directory:bowerDir, cmd:'update'})
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir task
+ |--------------------------------------------------------------------------
+ */
+
+//sourcemaps preferances
 elixir.config.sourcemaps = false;
 
+//GULP running task
 elixir(function(mix) {
+
+    //JADE settings
     mix.jade({
         baseDir: './resources',
         src: '/assets/jade/',
@@ -13,11 +41,37 @@ elixir(function(mix) {
         blade: true
     });
 
+    //Path LESS and JS production and CSS & JS production
+    var lessDir  = './resources/assets/less';
+    var pubCSS = 'public/css';
+    var pubJS = 'public/js';
 
-    mix.less(["resources/assets/less/pages/home.less"], "public/css/home.css");//delete less
+
+    //LESS and JS settings
+    mix.less([ lessDir + '/pages/home.less'], pubCSS + "home.css");//delete less
 
 
-    mix.scripts(["components/layout.js", "pages/home.js"], "public/js/home.min.js");
+    mix.scripts(["components/layout.js", "pages/home.js"], pubJS + "home.min.js");
 
+
+
+    //setting Bower resources copy
+    if (elixir.config.production) {
+
+        //Path directory resources compiled
+        var lib = '.public/libs';
+
+        //CSS libraries
+        mix.styles([
+            bowerDir + "/css/font-awesome.min.css",
+            bowerDir + "/select2-bootstrap.min.css"
+        ], lib + '/css/bower.css');
+
+        //JS libraries
+        mix.scripts([
+            bowerDir + "/jquery.js",
+            bowerDir + "palette.js"
+        ], lib + '/js/bower.js');
+    }
 
 });
